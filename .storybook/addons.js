fd50586342355521f6606cfb2ae19d24efefcad5
Edd-Strickland <estrickland@yoobic.com>
2019-11-01 12:04:37 +0000

    feat(stencilbook): storybook files added

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,1 +1,1 @@
-
+// File to add addons however you can also use presets
--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,5 @@
 import { configure } from '@storybook/html';
 
-// configure(require.context('../src', true, /\.stories\.js$/), module);
-
 const req = require.context('../src', true, /\.stories\.ts$/);
 
 configure(req, module);
