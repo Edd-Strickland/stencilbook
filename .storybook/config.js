5400f29cff3ac437800ef30f277fd3f0b59ef577
Michel Boudreau <michelboudreau@gmail.com>
2019-11-18 09:48:28 +1100

    Adding typescript support to storybook, specifying sass preset, adding ts|tsx files to be supported for stories, adding start script in package.json

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,5 @@
 import { configure } from '@storybook/html';
 
-const req = require.context('../src', true, /\.stories\.ts$/);
+const req = require.context('../src', true, /\.stories\.tsx?$/);
 
 configure(req, module);
